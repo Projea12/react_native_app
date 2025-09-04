06409171a5e1ad1ef822530a15726473c13a61c6
John Olugbemi <jjohnayo17@gmail.com>
2025-09-04 15:05:03 +0100

    search_screen

---
+++ src/presentation/screens/SearchScreen.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native'
+import React,{useEffect, useState} from 'react'
+
+const SearchScreen = () => {
+    const [isListening, setListining] = useState(false);
+    
+  return (
+    <View>
+      <Text>SearchScreen</Text>
+    </View>
+  )
+}
+
+export default SearchScreen
+
